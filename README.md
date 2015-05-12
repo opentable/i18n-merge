893793116a9c3edf45236f45b900a4eb441869f8
Steve Higgs <shiggs@opentable.com>
2015-05-12 13:58:46 +0100

    add tests and docs

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# i18n-merge
+#i18n-merge
 Merge together objects according to their languages
+
+## Methods
+
+### `.merge({source}, [languages], options);`
+
+#### source
+
+The source object to be transformed must include an `i18n` field, with the following schema:
+
+```
+{
+    ...
+    "i18n": [
+        {
+            ...
+            "language": {
+                "code": "en",  // two letter language code
+                "region": "GB" // iso8601 region code
+            },
+            ...
+        },
+        ...
+    ],
+    ...
+}
+```
+
+The source can either be a single object or an array of objects.
+
+####languages
+
+The languages array should be an array of objects with the following schema:
+
+```
+[
+    {
+        "code": "en",
+        "region": "GB",
+        "quality": 1.0
+    }
+]
+```
+
+(the accept-language-parser module can break apart an accept language string and return it in the correct format)
+
+
+####options
+
+The options object can consist of any of the following:
+
+```
+{
+    fnI18nLocator: function(source) //get the i18n array out of the source object, defaults to returning i18n
+    fnLanguageLocator: function(i18nSource) //get the language out of the i18n object. Defaults to returning language
+    fnPostMerge: function(source, mergedI18n) //what to do with the merged object, defaults to replaceing the i18n array
+    autoLanguageFallback: bool //should we fallback to parent languages (en-US -> en) automatically, defaults to true
+}
+```
+
+By overriding the optional functions this module should be able to work with most structures
+
+
+
--- tests/tests.js
+++ tests/tests.js
@@ -1,2 +1,104 @@
-var i18n = require('../lib/i18n-merge');
+var i18n = require('../lib/i18n-merge'),
+    _ = require('lodash');
+
 should = require('should');
+
+describe('i18n tests', function(){
+
+    var testObj = {
+        filddd: "tt",
+        I18n: [
+            {
+                Name: "gonna drink some beer and shoot some stuff y'all",
+                ukval: null,
+                Language: {
+                    IETF: "en-US",
+                    Code: "en",
+                    Region: "US"
+                }
+            },
+            {
+                Name: "pip pip tally ho crumpets and tea",
+                ukval: "uk",
+                Language: {
+                    IETF: "en-GB",
+                    Code: "en",
+                    Region: "GB"
+                }
+            },
+            {
+                Name: "common de",
+                commonVal: "common de",
+                Language: {
+                    IETF: "de",
+                    Code: "de",
+                    Region: null
+                }
+            },
+            {
+                Name: "specific de",
+                Language: {
+                    IETF: "de-DE",
+                    Code: "de",
+                    Region: 'DE'
+                }
+            },
+            {
+                Name: "common fr",
+                Language: {
+                    IETF: "fr",
+                    Code: "fr",
+                    Region: null
+                }
+            }
+        ]
+    };
+
+    it('should merge the fields for the specified language', function(){
+
+        var result = _.cloneDeep(testObj);
+        i18n.merge(result, [ { code: "en", region: "US", quality: 1.0 } ]);
+
+        result.I18n.Name.should.eql("gonna drink some beer and shoot some stuff y'all");
+        result.I18n.Language.IETF.should.eql("en-US");
+    });
+
+    it('should handle arrays of source objects', function(){
+        var result = []
+        result.push(_.cloneDeep(testObj));
+        result.push(_.cloneDeep(testObj));
+        i18n.merge(result, [ { code: "en", region: "US", quality: 1.0 } ]);
+
+        result.length.should.eql(2);
+        result[0].I18n.Name.should.eql("gonna drink some beer and shoot some stuff y'all");
+        result[1].I18n.Name.should.eql("gonna drink some beer and shoot some stuff y'all");
+    });
+
+    it('should automatically fallback to the parent language', function(){
+        var result = _.cloneDeep(testObj);
+        i18n.merge(result, [ { code: "fr", region: "FR", quality: 1.0 } ]);
+
+        result.I18n.Name.should.eql("common fr");
+    });
+
+    it('should automatically merge fields from the parent language', function(){
+        var result = _.cloneDeep(testObj);
+        i18n.merge(result, [ { code: "de", region: "DE", quality: 1.0 } ]);
+
+        result.I18n.commonVal.should.eql("common de");
+    });
+
+    it('should sort the language set based on quality', function(){
+        var result = _.cloneDeep(testObj);
+        i18n.merge(result, [ { code: "en", region: "GB", quality: 0.6 }, { code: "de", region: "DE", quality: 1.0 }  ]);
+
+        result.I18n.Name.should.eql("specific de");
+    });
+
+    it('should compare language codes in a case-insensitive manner', function(){
+        var result = _.cloneDeep(testObj);
+
+        i18n.merge(result, [ { code: "En", region: "Us", quality: 1.0 } ]);
+        result.I18n.Language.IETF.should.eql("en-US");
+    });
+});
